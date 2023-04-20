ca2b9645c865ea8940d0bf8b57de3974c162d3af
Erik Johnson <satirfist@gmail.com>
2023-04-20 07:48:27 -0700

    Intermediate check in of state and event stuff

--- main.py
+++ main.py
@@ -7,52 +7,114 @@
 import pygame
 import pygame_gui
 from spriteclasses import *
+from enum import Enum
 
-# pygame library needs to initalize itself
-pygame.init() 
-# name at the top of the window
-pygame.display.set_caption('Turbo!') 
-#window_surface = pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
-window_surface = pygame.display.set_mode((800, 600))
 
-# initalize the gui manager we want to use and HOLD ON to the object it creates
-manager = pygame_gui.UIManager((800, 600)) 
+#lets make a bunch of events
+SWITCH_TO_MENU          = pygame.event.custom_type()
+START_COUNTDOWN_TO_GAME = pygame.event.custom_type()
+START_GAME              = pygame.event.custom_type()
+END_GAME_PAUSE          = pygame.event.custom_type()
+QUIT_GAME               = pygame.event.custom_type() # shut it down
+QUIT_PLAY               = pygame.event.custom_type() # give up on play and return to menu
 
-# Create a button using our gui library
-Start_One = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((550, 550), (100, 50)), text='Start One', manager=manager)
-Start_Two = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((650, 550), (100, 50)), text='Start Duo', manager=manager)
-namePlayer1 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((20, 10), (150, 40)), initial_text= "Player 1", manager=manager)
-namePlayer2 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 10), (150, 40)), initial_text= "Player 2", manager=manager)
+#lets make sone states
+class GameStates(Enum):
+    SHOWING_MENU        = 1
+    COUNTDOWN_TO_GAME   = 2
+    PLAYING_GAME        = 3
+    SHOW_SCORE          = 4    
+    EXIT                = 5
+
+pygame.init()  # pygame library needs to initalize itself
+pygame.display.set_caption('Turbo!') # name at the top of the window
+
+
+window_surface = pygame.display.set_mode((800, 600)) #window_surface = pygame.display.set_mode((800, 600), pygame.FULLSCREEN)
+manager = pygame_gui.UIManager((800, 600)) # initalize the gui manager we want to use and HOLD ON to the object it creates
+
+# lets make containers to hold the controls for each 'screen'
+Menu_Container = pygame_gui.core.UIContainer(relative_rect=pygame.Rect((0,0),(800,600)), manager=manager)
+
+Start_One = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((550, 550), (100, 50)), container= Menu_Container, text='Start One', manager=manager)
+#Start_Two = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((650, 550), (100, 50)), container= Menu_Container,text='Start Duo', manager=manager)
+namePlayer1 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((20, 10), (150, 40)), container= Menu_Container,initial_text= "Player 1", manager=manager)
+#namePlayer2 = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((200, 10), (150, 40)), container= Menu_Container,initial_text= "Player 2", manager=manager)
+ExitGame = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((20, 560), (60, 30)), container= Menu_Container,text='Exit', manager=manager)
+
+Game_Container = pygame_gui.core.UIContainer(relative_rect=pygame.Rect((0,0),(800,600)), manager=manager)
+Score_Field = pygame_gui.elements.UILabel(relative_rect=pygame.Rect((350, 40), (100, 10)), container= Game_Container,text='0000', manager=manager)
+BackMenu = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((20, 560), (60, 30)), container= Game_Container,text='Back', manager=manager)
+
 
 ship = ShipSpriteClass(window_surface,"Ship_1.png","Ship_2.png","Ship_3.png", 200, 200)
 ship2 = ShipSpriteClass(window_surface,"Ship_1.png","Ship_2.png","Ship_3.png", 300, 300)
 ship3 = ShipSpriteClass(window_surface,"Ship_1.png","Ship_2.png","Ship_3.png", 100, 100)
+ship4 = ShipSpriteClass(window_surface,"Ship_1.png","Ship_2.png","Ship_3.png", 300, 100)
 coolbackground = BackgroundSpriteClass(window_surface, "background.png")
+
 # create a clock object so we can do thigs with time and things like elapsed time
 clock = pygame.time.Clock()
-# we are want to track cummulative time so we start with it as
-time_cumulative = 0
-# create a flag for running (and for exiting)
-is_running = True
+time_cumulative = 0 # we are want to track cummulative time so we start with it as
+is_running = True # create a flag for running (and for exiting)
+
+currentState = GameStates.SHOWING_MENU
+nextState = GameStates.SHOWING_MENU                     # used as temporary storage when changing states
+pygame.event.post(pygame.event.Event(SWITCH_TO_MENU))   # two things happening here, the inner argument creates the event, then we 'post it'
+
 #lets loop until we are told to quit by our is_running flag
-while is_running:
-    #lets get our our time in between 'frames' aka time since new image was displayed
-    time_delta = clock.tick(60)/1000.0
-    # keep track of cumulative time
-    time_cumulative = time_delta + time_cumulative
-    #did 'events' happen (such as button presses or exit hit)
-    for event in pygame.event.get():
+countdown_start = 0
+while is_running:    
+    time_delta = clock.tick(60)/1000.0              #lets get our our time in between 'frames' aka time since new image was displayed
+    time_cumulative = time_delta + time_cumulative  # keep track of cumulative time        
+    for event in pygame.event.get():                #did 'events' happen (such as button presses or exit hit)
+
+        if event.type == SWITCH_TO_MENU:
+            # in here we have to disable UI controls related to anything but the main menu
+            # and enable all the controls related to the main menu
+            Menu_Container.show()       
+            Game_Container.hide()
+            nextState = GameStates.SHOWING_MENU   
+        elif event.type == START_COUNTDOWN_TO_GAME:
+            # clear and restart the game structures (will display during countdown, dont want to see old data)
+            Menu_Container.hide()
+            Game_Container.show()
+            nextState = GameStates.COUNTDOWN_TO_GAME   
+            countdown_start = time_cumulative 
+            pygame.time.set_timer(pygame.event.Event(START_GAME), 4000) # countdown timer           
+        elif event.type == START_GAME:
+            Menu_Container.hide()
+            Game_Container.show()
+            nextState = GameStates.PLAYING_GAME 
+        elif event.type == QUIT_PLAY: # give up
+            Menu_Container.show()
+            Game_Container.hide()
+            nextState = GameStates.SHOWING_MENU 
+        elif event.type == END_GAME_PAUSE:
+            Menu_Container.hide()
+            Game_Container.show()
+            nextState = GameStates.SHOW_SCORE 
+            countdown_start = time_cumulative 
+            pygame.time.set_timer(pygame.event.Event(SWITCH_TO_MENU), 4000) # 10 second countdown timer               
+        elif event.type == QUIT_GAME:
+            Menu_Container.hide()
+            Game_Container.show()
+            nextState = GameStates.EXIT                        
+
         # did someone hit quit (x at top left corner)
         if event.type == pygame.QUIT:
-            #inform and set the flag to not running so we quit next loop through
-            print('Lets quit.')
-            is_running = False
+            pygame.event.post(pygame.event.Event(QUIT_GAME))  
 
         #someone hit a button (could have more than one)
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
             #which button was hit...was it this one?
             if event.ui_element == Start_One:
-                is_running = False
+                pygame.event.post(pygame.event.Event(START_COUNTDOWN_TO_GAME))
+            if event.ui_element == BackMenu:
+                pygame.event.post(pygame.event.Event(QUIT_PLAY))
+            if event.ui_element == ExitGame:
+                pygame.event.post(pygame.event.Event(QUIT_GAME))                
+            
         
         #this frame could have more than one event so lets process/get any other that may exist
         manager.process_events(event)
@@ -65,10 +127,14 @@ while is_running:
     ship.draw(time_cumulative)    
     ship2.draw(time_cumulative+ 1.3)  
     ship3.draw(time_cumulative+ 2.7) 
+    ship4.draw(time_cumulative+ .7) 
     
     manager.draw_ui(window_surface)
     # swap the background image we have been drawing on to the screen
     pygame.display.update() 
-
+    currentState = nextState # we had a state change, lets set it for next iteration of the look
+    if currentState == GameStates.EXIT:
+        is_running = False
+        
 # deactivates the pygame library
 pygame.quit()
